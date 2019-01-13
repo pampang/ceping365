9d974a568693b86ca6e2d8ae4edacb3eff6fcbae
龐校長 <fspwz@foxmail.com>
2019-01-13 19:39:19 +0800

    修复 npm run build 报错的问题

--- pages/index.js
+++ pages/index.js
@@ -1,108 +1,11 @@
-import React from 'react'
-import Router from 'next/router'
 import Head from 'next/head'
 
-import Modal from '../components/modal'
-
-export default class extends React.Component {
-  static async getInitialProps({ req }) {
-    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
-    return {
-      userAgent,
-      photos: new Array(15).fill(0).map((v, k) => k + 1)
-    }
-  }
-
-  constructor (props) {
-    super(props)
-    this.onKeyDown = this.onKeyDown.bind(this)
-  }
-
-  // handling escape close
-  componentDidMount () {
-    document.addEventListener('keydown', this.onKeyDown)
-  }
-
-  componentWillUnmount () {
-    document.removeEventListener('keydown', this.onKeyDown)
-  }
-
-  onKeyDown (e) {
-    if (!this.props.url.query.photoId) return
-    if (e.keyCode === 27) {
-      this.props.url.back()
-    }
-  }
-
-  dismissModal () {
-    Router.push('/')
-  }
-
-  showPhoto (e, id) {
-    e.preventDefault()
-    Router.push(`/?photoId=${id}`, `/photo?id=${id}`)
-  }
-
-  render () {
-    const { url, photos } = this.props
-
-    return (
-      <div className='list'>
-        <Head>
-          <title>My page title</title>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
-        </Head>
-        <div>
-          Hello World {this.props.userAgent}
-        </div>
-        {
-          url.query.photoId &&
-            <Modal
-              id={url.query.photoId}
-              onDismiss={() => this.dismissModal()}
-            />
-        }
-        {
-          photos.map((id) => (
-            <div key={id} className='photo'>
-              <a
-                className='photoLink'
-                href={`/photo?id=${id}`}
-                onClick={(e) => this.showPhoto(e, id)}
-              >
-                {id}
-              </a>
-            </div>
-          ))
-        }
-        <style jsx>{`
-          .list {
-            padding: 50px;
-            text-align: center;
-          }
-
-          .photo {
-            display: inline-block;
-          }
-
-          .photoLink {
-            color: #333;
-            verticalAlign: middle;
-            cursor: pointer;
-            background: #eee;
-            display: inline-block;
-            width: 250px;
-            height: 250px;
-            line-height: 250px;
-            margin: 10px;
-            border: 2px solid transparent;
-          }
-
-          .photoLink:hover {
-            borderColor: blue;
-          }
-        `}</style>
-      </div>
-    )
-  }
-}
+export default () => (
+  <div>
+    <Head>
+      <title>My page title</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <p>Hello world!</p>
+  </div>
+)
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const next = require('next')
 const Router = require('koa-router');
 var bodyParser = require('koa-bodyparser');
 
-const port = parseInt(process.env.PORT, 10) || 3000
+const port = parseInt(process.env.PORT, 10) || 80
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
